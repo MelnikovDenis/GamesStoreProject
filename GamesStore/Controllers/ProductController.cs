b2626521c6ce6cbd0e32e4a3c3d1015262380b02
Denis Melnikov <sinennn@yandex.ru>
2023-03-07 04:00:10 +0500

    Добавлено разбиение на страницы

--- GamesStore/Controllers/ProductController.cs
+++ GamesStore/Controllers/ProductController.cs
@@ -3,10 +3,17 @@ using GamesStore.Models;
 namespace GamesStore.Controllers;
 public class ProductController : Controller
 {
-    private IProductRepository repository;
+    private IProductRepository Repository {get; set;}
+    public int PageSize {get; set;} = 4;
     public ProductController(IProductRepository repository) 
     {
-        this.repository = repository;
+        this.Repository = repository;
     }
-    public IActionResult List() => View(repository.Products);
+    public IActionResult List(int productPage)
+    {
+        return View((from product in Repository.Products
+            orderby product.ProductID
+            select product).Skip((productPage - 1) * PageSize).Take(PageSize)
+        );
+    }
 }
